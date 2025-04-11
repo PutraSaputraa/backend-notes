5215a768ada6ce37a2ae5bfa7dc3d22f6b90261a
PutraSaputraa <161487538+PutraSaputraa@users.noreply.github.com>
2025-04-11 19:38:07 +0700

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -2,8 +2,7 @@ import { Sequelize } from "sequelize";
 
 const db = new Sequelize("myouinotes", "databaseku", "12345678", {
     host: "34.55.168.45",
-    dialect: "mysql",
-    port: "3306"
+    dialect: "mysql"
 });
 
 export default db;
