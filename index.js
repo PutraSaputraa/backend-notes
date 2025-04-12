98fd1442b3fef3a948512de572aa099cf2728a1d
PutraSaputraa <161487538+PutraSaputraa@users.noreply.github.com>
2025-04-12 14:46:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ import express from "express"
 import cors from "cors"
 import router from "./route/noteRoute.js"
 
+const PORT = process.env.PORT || 5000;
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(router);
 
 // app.listen(5000, ()=> console.log("Server Telah Berjalan"));
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
