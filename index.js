20bfff03b655aae0849763031049f7a70f4f6196
PutraSaputraa <161487538+PutraSaputraa@users.noreply.github.com>
2025-04-12 12:01:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-// app.listen(4000, ()=> console.log("Server Telah Berjalan"));
+app.listen(4000, ()=> console.log("Server Telah Berjalan"));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
